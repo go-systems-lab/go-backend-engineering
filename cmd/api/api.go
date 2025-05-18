3f104bb281f95cf945e6cdec825f8595370eb589
kuluruvineeth <kuluruvineeth8623@gmail.com>
2025-05-18 10:43:06 +0530

    - add API server implementation with health check endpoint

---
+++ cmd/api/api.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+}
+
+func (app *application) mount() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /v1/health", app.healthcheckHandler)
+
+	return mux
+}
+
+func (app *application) run(mux *http.ServeMux) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  10 * time.Second,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Println("Starting server on", app.config.addr)
+
+	return srv.ListenAndServe()
+}
