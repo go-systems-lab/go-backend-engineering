4b1dd08e755cc5aa38656eda4c9cf3adc821c047
kuluruvineeth <kuluruvineeth8623@gmail.com>
2025-05-18 11:28:23 +0530

    - Implement storage layer with Posts and Users stores; update API application to use new storage structure

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,14 +4,17 @@ import (
 	"log"
 
 	"github.com/kuluruvineeth/social-go/internal/env"
+	"github.com/kuluruvineeth/social-go/internal/store"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 	}
+	store := store.NewStorage(nil)
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	mux := app.mount()
