3bb69f4247658ed839afd3d9b2bc1d66aac1f7dc
kuluruvineeth <kuluruvineeth8623@gmail.com>
2025-05-18 12:01:28 +0530

    - Add PostgreSQL database configuration and connection management; introduce docker-compose for local development

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/kuluruvineeth/social-go/internal/db"
 	"github.com/kuluruvineeth/social-go/internal/env"
 	"github.com/kuluruvineeth/social-go/internal/store"
 )
@@ -10,8 +11,23 @@ import (
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social_go?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdletime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
-	store := store.NewStorage(nil)
+
+	db, err := db.NewDB(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdletime)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+	log.Println("Connected to the database")
+
+	store := store.NewStorage(db)
 	app := &application{
 		config: cfg,
 		store:  store,
