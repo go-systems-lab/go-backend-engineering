245179260f09a50e83c927edfb17ab225528f87b
kuluruvineeth <kuluruvineeth8623@gmail.com>
2025-05-19 11:12:30 +0530

    - Refactor error handling in API endpoints to use dedicated error functions; add new error handling methods for internal server, not found, and bad request errors in the application package.

---
+++ cmd/api/errors.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("internal server error: %s path: %s error: %s", r.Method, r.URL.Path, err)
+	writeJSONError(w, http.StatusInternalServerError, err.Error())
+}
+
+func (app *application) notFoundError(w http.ResponseWriter, r *http.Request) {
+	log.Printf("not found: %s path: %s error: %s", r.Method, r.URL.Path, "not found")
+	writeJSONError(w, http.StatusNotFound, "not found")
+}
+
+func (app *application) badRequestError(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("bad request: %s path: %s error: %s", r.Method, r.URL.Path, err)
+	writeJSONError(w, http.StatusBadRequest, err.Error())
+}
