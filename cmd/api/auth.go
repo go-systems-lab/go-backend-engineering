896a0137fdcb98fc871f438809a647a37f575f8c
kuluruvineeth <kuluruvineeth8623@gmail.com>
2025-05-20 16:15:05 +0530

    - Add user registration API: implement registerUserHandler for user registration, including input validation and password hashing. Create user invitations and activation status migrations. Update storage interface and user model to support new functionality.

---
+++ cmd/api/auth.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/kuluruvineeth/social-go/internal/store"
+)
+
+type RegisterUserPayload struct {
+	Username string `json:"username" validate:"required,max=100`
+	Email    string `json:"email" validate:"required,email,max=255"`
+	Password string `json:"password" validate:"required,min=3,max=72"`
+}
+
+// registerUserHandler godoc
+//
+//	@Summary		Registers a user
+//	@Description	Registers a user
+//	@Tags			authentication
+//	@Accept			json
+//	@Produce		json
+//	@Param			payload	body		RegisterUserPayload	true	"User credentials"
+//	@Success		201		{object}	UserWithToken		"User registered"
+//	@Failure		400		{object}	error
+//	@Failure		500		{object}	error
+//	@Router			/authentication/user [post]
+func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
+	var payload RegisterUserPayload
+	if err := readJSON(w, r, &payload); err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
+
+	if err := validate.Struct(payload); err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
+
+	user := &store.User{
+		Username: payload.Username,
+		Email:    payload.Email,
+	}
+
+	// hash password
+	if err := user.Password.Set(payload.Password); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+
+	// store the user
+	if err := app.store.Users.CreateAndInvite(r.Context(), user, "token-123"); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+
+	if err := app.jsonResponse(w, http.StatusCreated, nil); err != nil {
+		app.internalServerError(w, r, err)
+	}
+
+}
