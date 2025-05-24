3d3bf371a837fde7451d02c6755d558e924faf37
kuluruvineeth <kuluruvineeth8623@gmail.com>
2025-05-24 10:11:07 +0530

    - fix syntax issue

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -14,7 +14,7 @@ import (
 )
 
 type RegisterUserPayload struct {
-	Username string `json:"username" validate:"required,max=100`
+	Username string `json:"username" validate:"required,max=100"`
 	Email    string `json:"email" validate:"required,email,max=255"`
 	Password string `json:"password" validate:"required,min=3,max=72"`
 }
